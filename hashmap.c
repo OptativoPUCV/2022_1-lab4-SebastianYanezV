57ad7b580c2e6fdf4e9b27d4414b918849783bcf
Your Name <you@example.com>
2022-04-08 11:43:23 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -51,18 +51,17 @@ void insertMap(HashMap * map, char * key, void * value)
         map->current = pos;
         map->size++;
     }
-    /*else
+    else
     {
         while (map->buckets[pos] != NULL)
         {
             pos++;
         }
         
-        strcpy(map->buckets[pos]->key, key);
-        map->buckets[pos]->value = value;
+        map->buckets[pos] = newPair;
         map->current = pos;
         map->size++;
-    }*/
+    }
 }
 
 void enlarge(HashMap * map) {
