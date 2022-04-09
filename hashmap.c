25cacb878c6b2f020d96acb70b75369e1e931613
Your Name <you@example.com>
2022-04-08 22:04:18 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -73,12 +73,23 @@ void insertMap(HashMap * map, char * key, void * value)
     }
 }
 
-void enlarge(HashMap * map) {
+void enlarge(HashMap * map) 
+{
     enlarge_called = 1; //no borrar (testing purposes)
 
+    Pair **oldBuckets = (Pair **) malloc(map->capacity * sizeof(Pair *));
+    oldBuckets = map->buckets;
+    long largoOldBuckets = map->size;
 
+    map->capacity *= 2;
+    map->buckets = (Pair **) malloc(map->capacity * sizeof(Pair *));
+    map->size = 0;
+
+    for (int i = 0 ; i < largoOldBuckets ; i++)
+    {
+        insertMap(map, oldBuckets[i]->key, oldBuckets[i]->value);
+    }
 }
-
 
 HashMap * createMap(long capacity) 
 {
