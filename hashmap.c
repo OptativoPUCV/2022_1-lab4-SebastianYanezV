ab96da79ded51d4261cc8361cd22b067dcad4583
Your Name <you@example.com>
2022-04-09 12:03:07 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -114,11 +114,12 @@ void eraseMap(HashMap * map,  char * key)
     }
     else
     {
-        /*while (map->buckets[pos] != NULL && map->buckets[pos]->key != NULL)
-        {*/
-            if (is_equal(map->buckets[pos]->key, key) == 1);
+        while (map->buckets[pos] != NULL)
+        {
+            //&& map->buckets[pos]->key != NULL
+            if (is_equal(map->buckets[pos]->key, key) == 1) break;
             pos = (pos + 1) % map->capacity;
-        //}
+        }
 
         map->buckets[pos]->key = NULL;
         map->size--;
